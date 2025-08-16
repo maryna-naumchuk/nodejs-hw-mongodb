561e57df91511ffdc34cf68ed530b5986cd2a398
Maryna <marin.naumch@gmail.com>
2025-08-16 21:21:51 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import { TMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/contacts.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
-
-  await createDirIfNotExists(TMP_UPLOAD_DIR);
-  await createDirIfNotExists(UPLOAD_DIR);
-
   setupServer();
 };
 
-void bootstrap();
+bootstrap();
